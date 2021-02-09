2a39878266575c9405c0a2fb891af465510d1a08
Janu <janu.m.sung@gmail.com>
2021-02-08 20:26:33 -0800

    first commit

---
+++ src/functions.js
@@ -0,0 +1,16 @@
+// const moreAboutMeSection = document.getElementById(".moreAboutMeSection");
+
+function showMoreAboutMe() {
+  document.getElementById("moreAboutMeSection").style.display = "grid";
+  document.getElementById("showLessAboutMeBtn").style.display = "block";
+  document.getElementById("about").style.margin = "30px 0px";
+  document.getElementById("showMoreAboutMeBtn").style.display = "none";
+}
+
+function showLessAboutMe() {
+  document.getElementById("moreAboutMeSection").style.display = "none";
+  document.getElementById("showMoreAboutMeBtn").style.display = "block";
+  document.getElementById("about").style.margin = "40px 0px";
+  document.getElementById("about").style.margin = null;
+  document.getElementById("showLessAboutMeBtn").style.display = "none";
+}
